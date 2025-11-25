e6b478d8004b362240adbae7e1f42394f742bf93
Danny Dinglasa Jr. <danyxtu020@gmail.com>
2025-11-25 12:57:15 +0800

    asda

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,3 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('user', function (Request $req) {
     return $req->user();
 });
-
-
-Route::get('');
